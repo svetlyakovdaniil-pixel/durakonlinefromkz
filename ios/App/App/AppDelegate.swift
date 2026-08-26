2beadad4c4d6c2c56d094d86229411cea591ffe4
svetlyakovdaniil-pixel <svetlyakovdaniil@gmail.com>
2026-08-27 02:23:29 +0500

    fix(ios): configure Firebase before bridge startup

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Capacitor
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = MyViewController()
         window.makeKeyAndVisible()
